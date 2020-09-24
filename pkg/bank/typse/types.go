f1f96a720e225f6f7a2545cca5ebdcd223fecff9
Firdavs <FirdavsUrunov>
2020-09-24 22:58:29 +0500

    Начальная версия

---
+++ pkg/bank/typse/types.go
@@ -0,0 +1,33 @@
+package types
+
+// Money представляет собой денеэную сумму в минималных единиц (центы, копейки, дирамы и т.д)
+type Money int64
+
+// Category представляет собой категорию, в которию был совершен платёж (авто, аптека, рестараны и т.д)
+type Category string
+
+// Currency представляет код валюты
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Card представляет информацию о платежной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Payment    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
