623085cb94d9a08bf72756701451459c5e6a1f91
Firdavs <FirdavsUrunov>
2020-09-24 23:30:25 +0500

    удалено поле MinBalance из структуры карты

--- pkg/bank/typse/types.go
+++ pkg/bank/typse/types.go
@@ -22,13 +22,12 @@ type PAN string
 
 // Card представляет информацию о платежной карте
 type Card struct {
-	ID         int
-	PAN        PAN
-	Balance    Money
-	MinBalance Money
-	Payment    Money
-	Currency   Currency
-	Color      string
-	Name       string
-	Active     bool
+	ID       int
+	PAN      PAN
+	Balance  Money
+	Payment  Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
 }
